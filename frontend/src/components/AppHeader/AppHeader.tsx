3fa92a289a0fc454a9ebaef6359b2ad8203f53cf
Lim Li <limlirandom@gmail.com>
2022-01-13 15:56:17 +0800

    chore: add app header

---
+++ frontend/src/components/AppHeader/AppHeader.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Button, HStack, Text } from '@chakra-ui/react'
+
+import { useAuth } from '~/auth'
+
+const AppHeader: React.FC = () => {
+  const { auth, setAuth } = useAuth()
+
+  const onLogout = () => {
+    setAuth(null)
+  }
+
+  return (
+    <HStack
+      padding="20px 32px"
+      background="white"
+      borderBottomWidth="1px"
+      borderColor="neutral.300"
+      borderStyle="solid"
+      justify="space-between"
+      align="center"
+    >
+      <Text>Spotlight logo here</Text>
+      <HStack align="center" spacing="16px" paddingRight="16px">
+        <Text textStyle="subhead1" color="neutral.700">
+          {auth?.user.email}
+        </Text>
+        <Button variant="outline" onClick={onLogout}>
+          Logout
+        </Button>
+      </HStack>
+    </HStack>
+  )
+}
+
+export default AppHeader
--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -1,7 +1,9 @@
 import { Link } from 'react-router-dom'
-import { Button, ButtonGroup, Flex } from '@chakra-ui/react'
+import { Button, ButtonGroup, Container, Flex } from '@chakra-ui/react'
 
 import { useAuth } from '~/auth'
+
+import AppHeader from '~components/AppHeader'
 
 const DashboardPage = (): JSX.Element => {
   const { auth, setAuth } = useAuth()
@@ -11,18 +13,23 @@ const DashboardPage = (): JSX.Element => {
   }
 
   return (
-    <Flex flexDir="column">
-      YOU ARE NOW AUTHENTICATED as {auth?.user.email}. Replace this page with
-      the root page of your application.
-      <ButtonGroup>
-        <Button onClick={logout}>Logout</Button>
-        <Button>
-          <Link to="/login">
-            Attempt to go to login page (and see nothing happen)
-          </Link>
-        </Button>
-      </ButtonGroup>
-    </Flex>
+    <>
+      <AppHeader />
+      <Container>
+        <Flex flexDir="column">
+          YOU ARE NOW AUTHENTICATED as {auth?.user.email}. Replace this page
+          with the root page of your application.
+          <ButtonGroup>
+            <Button onClick={logout}>Logout</Button>
+            <Button>
+              <Link to="/login">
+                Attempt to go to login page (and see nothing happen)
+              </Link>
+            </Button>
+          </ButtonGroup>
+        </Flex>
+      </Container>
+    </>
   )
 }
 
