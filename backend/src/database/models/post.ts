0532c5bf5d9369c011c5ae42c2565f7ef5496843
Jerald <jerald@Jeralds-MacBook-Pro.local>
2022-01-14 16:26:45 +0800

    feat: add post model schema

---
+++ backend/src/database/models/post.ts
@@ -0,0 +1,43 @@
+import {
+  Column,
+  CreatedAt,
+  DataType,
+  Model,
+  Table,
+  UpdatedAt,
+} from 'sequelize-typescript'
+
+@Table({ tableName: 'posts' })
+export class Post extends Model {
+  @Column({
+    primaryKey: true,
+    type: DataType.INTEGER,
+    autoIncrement: true,
+  })
+  id!: number
+
+  @Column({
+    type: DataType.TEXT,
+    unique: true,
+    allowNull: false,
+  })
+  userId!: number
+
+  @Column({
+    type: DataType.TEXT,
+    allowNull: false,
+  })
+  issue!: string
+
+  @Column({
+    type: DataType.TEXT,
+    allowNull: false,
+  })
+  actionsTaken!: string
+
+  @CreatedAt
+  createdAt!: Date
+
+  @UpdatedAt
+  updatedAt!: Date
+}
