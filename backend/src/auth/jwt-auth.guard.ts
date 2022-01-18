0b10b3a5d0eeed6f6dbc04442ab00873dc4532d4
JasonChong96 <jasontjakra@gmail.com>
2022-01-18 15:20:59 +0800

    fix: removes tests

--- backend/src/auth/jwt-auth.guard.ts
+++ backend/src/auth/jwt-auth.guard.ts
@@ -1,7 +1,7 @@
 import { ExecutionContext, Injectable } from '@nestjs/common'
 import { Reflector } from '@nestjs/core'
 import { AuthGuard } from '@nestjs/passport'
-import { IS_PUBLIC_KEY } from 'helper'
+import { IS_PUBLIC_KEY } from '../helper'
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
