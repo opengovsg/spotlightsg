4c608c560bcb967cef2c23f2103c1041f9c17dd5
Jerald <jerald@Jeralds-MacBook-Pro.local>
2022-01-20 17:26:20 +0800

    chore: account for custom HttpException error messages

--- backend/src/middleware/errorHandler.ts
+++ backend/src/middleware/errorHandler.ts
@@ -9,28 +9,38 @@ import { BaseExceptionFilter } from '@nestjs/core'
 import { Response } from 'express'
 import _ from 'lodash'
 
+/**
+ * Error Handler middleware that abstracts out error handling from controller and service classes.
+ * Note that to retrieve the error message, there are 3 structures to handle:
+ * 1. error.response.message (from class-validator)
+ * 2. error.message (from sequelize errors)
+ * 3. error.response (from throwing custom HttpException using nestjs)
+ */
 @Catch()
 export class ErrorHandler extends BaseExceptionFilter {
   determineStatusCode(error: unknown): number {
     if (error instanceof HttpException) return error.getStatus()
     switch (_.get(error, 'name')) {
       case 'SequelizeForeignKeyConstraintError':
-        return HttpStatus.BAD_REQUEST
+        return HttpStatus.NOT_FOUND
       default:
         return HttpStatus.INTERNAL_SERVER_ERROR
     }
+  }
+  determineErrorMessage(error: unknown): string {
+    const response = _.get(error, 'response')
+    if (!response) return _.get(error, 'message')
+    if (typeof response === 'string') return response
+    return _.get(response, 'message')
   }
   catch(error: unknown, host: ArgumentsHost) {
     Logger.error(error)
     const response = host.switchToHttp().getResponse<Response>()
     const statusCode = this.determineStatusCode(error)
-    const responseFromClassValidator = _.get(error, 'response')
     response.status(statusCode).json({
       statusCode,
       errorName: _.get(error, 'name'),
-      message: responseFromClassValidator
-        ? _.get(responseFromClassValidator, 'message')
-        : _.get(error, 'message'),
+      message: this.determineErrorMessage(error),
     })
   }
 }
