32857e2bc6eae4a94431d9b3d3d44ced3ef22ade
Jerald <jerald@Jeralds-MacBook-Pro.local>
2022-01-25 17:17:14 +0800

    chore: remove unwanted module imports from post module

--- backend/src/posts/posts.module.ts
+++ backend/src/posts/posts.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common'
 import { SequelizeModule } from '@nestjs/sequelize'
-import { CommentsModule } from 'comments/comments.module'
-import { UpvoteModule } from 'upvotes/upvotes.module'
 import { Post } from '../database/models'
 import { PostsController } from './posts.controller'
 import { PostsService } from './posts.service'
 
 @Module({
-  imports: [SequelizeModule.forFeature([Post]), CommentsModule, UpvoteModule],
+  imports: [SequelizeModule.forFeature([Post])],
   providers: [PostsService],
   exports: [PostsService],
   controllers: [PostsController],
