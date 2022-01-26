345f9aa5296453f5505155f00dac2891b7f64184
JasonChong96 <jasontjakra@gmail.com>
2022-01-26 14:44:15 +0800

    fix: builds

--- backend/src/database/config.ts
+++ backend/src/database/config.ts
@@ -3,12 +3,10 @@ import { ConfigService } from '../config/config.service'
 const config = new ConfigService()
 
 module.exports = {
-  development: {
-    dialect: 'postgres',
-    host: config.get('db.host'),
-    port: config.get('db.port'),
-    username: config.get('db.username'),
-    password: config.get('db.password'),
-    database: config.get('db.database'),
-  },
+  dialect: 'postgres',
+  host: config.get('db.host'),
+  port: config.get('db.port'),
+  username: config.get('db.username'),
+  password: config.get('db.password'),
+  database: config.get('db.database'),
 }
