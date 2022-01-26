8a78b2ae1b4ad1779689d422b512826c9cc2032f
Lim Li <limlirandom@gmail.com>
2022-01-26 11:17:48 +0800

    chore: modify landing

--- frontend/src/pages/Landing/Landing.tsx
+++ frontend/src/pages/Landing/Landing.tsx
@@ -1,14 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import {
-  Box,
-  Button,
-  HStack,
-  Image,
-  SimpleGrid,
-  Text,
-  VStack,
-} from '@chakra-ui/react'
+import { Box, Button, Image, SimpleGrid, Text, VStack } from '@chakra-ui/react'
 
 import { LOGIN_ROUTE } from '~constants/routes'
 import AppHeader from '~components/AppHeader'
@@ -21,24 +13,25 @@ const Landing = (): JSX.Element => {
     <>
       <AppHeader />
       <SimpleGrid w="full" columns={2}>
-        <VStack pl="56px" pt="56px" align="start">
+        <VStack p="56px" align="start">
           <Text textStyle="display2" color="primary.400">
             Shining a spotlight on problems faced by public officers
           </Text>
-          <VStack pt="30px" align="start">
-            <HStack>
-              <Text textStyle="h3" color="primary.500">
-                200
-              </Text>
-              <Text textStyle="h4">Problems Surfaced</Text>
-            </HStack>
-            <HStack>
-              <Text textStyle="h3" color="primary.500">
-                100
-              </Text>
-              <Text textStyle="h4">Replies From Other Public Officers</Text>
-            </HStack>
-          </VStack>
+          <SimpleGrid
+            py="30px"
+            templateColumns="100px 1fr"
+            gap="10px"
+            alignItems="center"
+          >
+            <Text textStyle="h1" color="primary.500">
+              200
+            </Text>
+            <Text textStyle="h2">Problems Surfaced</Text>
+            <Text textStyle="h1" color="primary.500">
+              100
+            </Text>
+            <Text textStyle="h2">Replies From Other Public Officers</Text>
+          </SimpleGrid>
           <Button as={Link} to={LOGIN_ROUTE} colorScheme="primary">
             Find help for your problems
           </Button>
